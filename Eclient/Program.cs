03d4068620855e06a8a31e340d346d2a7718dcd1
Peter Levinsky <pele@easj.dk>
2017-08-24 19:24:07 +0200

    Version1 server og client virker med simple tekst

--- Eclient/Program.cs
+++ Eclient/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,19 @@ namespace Eclient
     {
         static void Main(string[] args)
         {
+            using (TcpClient socket = new TcpClient("localhost", 7777))
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamWriter sw = new StreamWriter(ns))
+            using (StreamReader sr = new StreamReader(ns))
+            {
+                String outline = "Peter";
+                sw.WriteLine(outline);
+                sw.Flush();
+
+                String inline = sr.ReadLine();
+                Console.WriteLine(inline);
+                Console.ReadLine(); // wait to finish
+            }
         }
     }
 }
--- Eserver/Program.cs
+++ Eserver/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,20 @@ namespace Eserver
     {
         static void Main(string[] args)
         {
+
+            TcpListener server = new TcpListener(IPAddress.Loopback,7777);
+            server.Start();
+
+            using (TcpClient socket = server.AcceptTcpClient())
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamWriter sw = new StreamWriter(ns))
+            using (StreamReader sr = new StreamReader(ns))
+            {
+                String inline = sr.ReadLine();
+                String outline = inline.ToUpper().Trim();
+                sw.WriteLine(outline);
+                sw.Flush();
+            }
         }
     }
 }
